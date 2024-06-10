d7956e89bdab1ea7bf4a57ca00bff3d14184331d
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-10 20:31:08 +0000

    added more rows, more info

--- pages/index.js
+++ pages/index.js
@@ -21,24 +21,31 @@ export default function Home() {
 
         <div className={styles.section_container}>
         <section className={styles.section}>
-          <p>We offer a diverse array of features, because our competitors simply have too much bullshit on them.</p>
-          <h2>Features</h2>
-          <ul>
-            <li>Free song uploads</li>
-            <li>Approval process</li>
-            <li>Discover new music from emerging artists</li>
-            <li>High-quality music</li>
-            <li>Free lyrics</li>
-            <li>Unlimited features</li>
-          </ul>
+          <h1>Introduction</h1>
+          <i>What we refer to in this page as "bullshit" means unnecessary and unwanted features that make the experience of using the service worse for a lot of users.</i>
+          <p>We have a robust and easy-to-use platform, that anyone can try.</p>
+          <p>We don't care about how much money you have in that wallet. We just want you to enjoy our service as much as possible.</p>
+          <p>With our approval-based song submission system, we make sure you only listen to songs that are made by users themselves.</p>
         </section>
 
         <section className={styles.section}>
            <h1>No bullshit</h1>
-           <p>We don't pull up huge paywalls everywhere, we don't make stupid interfaces. Our mission is to provide quality, and we will do that for as long as we possibly can.</p>
-          <p>We value our userbase more than anything, and we take suggestions openly. We aren't greedy, toxic, or deaf to our community like other services.</p>
+           <p>We don't pull up huge paywalls everywhere and we don't make stupid interfaces. Our mission is to provide quality, and we will do that for as long as we possibly can.</p>
+          <p>We value our userbase more than anything, and we take suggestions openly. We aren't greedy, toxic or deaf to our community like other services.</p>
         </section>
           </div>
+
+        <div className={styles.section_container}>
+          <section className={styles.section}>
+             <h1>About</h1>
+             <p>PenguinWaves was made by one online user called Gen1x, who wanted to have an honest and easy music listening service because he got annoyed of having to watch ads constantly.</p>
+            <p>So, he also wants YOU to not have to go through all that hussle.</p>
+          </section>
+          <section className={styles.section}>
+             <h1>How it works</h1>
+             <p>The main interface is powered by a project made using a platform called <a href="https://penguinmod.com/" target="_blank">PenguinMod</a> (hence the name "PenguinWaves").</p>
+          </section>
+            </div>
 
         <section className={styles.section}>
           <h2>So then... is it a "yes"?</h2>

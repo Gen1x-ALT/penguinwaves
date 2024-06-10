4c749b8127c9b86de3c4196ff4becdd5d1294573
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-10 03:10:42 +0000

    better homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '@styles/globals.css'
+import { useEffect } from 'react';
+import '@styles/globals.css';
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // Add the class to the body when the component mounts
+    document.body.classList.add('has-scrollbar');
+
+    // Remove the class from the body when the component unmounts
+    return () => {
+      document.body.classList.remove('has-scrollbar');
+    };
+  }, []); // Empty dependency array to only run the effect once
+
+  return <Component {...pageProps} />;
 }
 
-export default Application
+export default Application;
--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,17 @@ export default function Home() {
           </p>
         </section>
 
+        <div className={styles.section_container}>
         <section className={styles.section}>
+          <p>We offer a diverse array of features, because our competitors simply have too much bullshit on them.</p>
           <h2>Features</h2>
           <ul>
             <li>Free song uploads</li>
-            <li>Community approval process</li>
+            <li>Approval process</li>
             <li>Discover new music from emerging artists</li>
-            <li>High-quality streaming</li>
+            <li>High-quality music</li>
+            <li>Free lyrics</li>
+            <li>Unlimited features</li>
           </ul>
         </section>
 
@@ -34,6 +38,7 @@ export default function Home() {
            <p>We don't pull up huge paywalls everywhere, we don't make stupid interfaces. Our mission is to provide quality, and we will do that for as long as we possibly can.</p>
           <p>We value our userbase more than anything, and we take suggestions openly. We aren't greedy, toxic, or deaf to our community like other services.</p>
         </section>
+          </div>
 
         <section className={styles.section}>
           <h2>So then... is it a yes?</h2>

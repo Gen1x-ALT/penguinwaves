a8de330e6a41333acf365b2a91126134a52eb43c
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-11 16:45:57 +0000

    QUE RE MIL PORONGA ES UNA MILLAAAAAAAAAAAAAA!!!!!

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,18 @@
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>Made by <a href="https://gen1x.is-a.dev/">Gen1x</a> with <span style={{color: '#fea0ff'}}>love ❤</span></p>
+      <p>Made by <a href="https://gen1x.is-a.dev/">Gen1x</a> with <span style={{color: '#fea0ff'}}>love ❤</span> - Creado en Argentina <img src="https://flagsapi.com/AR/shiny/32.png" className="flag"></img></p>
       <style jsx>{`
         .footer {
           background-color: #121212;
           color: #ffffff;
           text-align: center;
           width: 100%;
+        }
+
+        .flag {
+          top: 11px;
+          position: relative;
         }
       `}</style>
     </footer>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import '@styles/globals.css';
+import { useRouter } from 'next/router';
 
 function Application({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
     // Add the class to the body when the component mounts
     document.body.classList.add('has-scrollbar');
@@ -10,6 +13,20 @@ function Application({ Component, pageProps }) {
     return () => {
       document.body.classList.remove('has-scrollbar');
     };
+  }, []); // Empty dependency array to only run the effect once
+
+  useEffect(() => {
+    // Check the current URL and perform the redirection if needed
+    const currentUrl = window.location.href;
+    const hostname = window.location.hostname;
+    const path = window.location.pathname;
+
+    if (
+      hostname === 'penguinwaves.netlify.app' &&
+      !currentUrl.endsWith('replit.dev')
+    ) {
+      window.location.href = `https://waves.mubi.tech${path}`;
+    }
   }, []); // Empty dependency array to only run the effect once
 
   return <Component {...pageProps} />;

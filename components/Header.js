ca72269f37f602b0b3a31acc7693bd507309845c
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-12 01:51:51 +0000

    add navbar and logo for later, also delete netlify logo

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,19 @@
-// @components/Header.js
+import { useRouter } from 'next/router';
+
 export default function Header({ title }) {
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <header>
+      <center><p>This entire platform is in its early stages. This means that you might find more errors.</p></center>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className={`nav-item ${pathname === '/' ? 'active' : ''}`}>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </nav>
       <h1 className="header-title">{title}</h1>
       <style jsx>{`
         .header-title {
@@ -14,7 +26,39 @@ export default function Header({ title }) {
           font-style: normal;
           text-align: center;
         }
+        .navbar {
+          background-color: #333;
+          overflow: hidden;
+          border-radius: 20px;
+        }
+        .nav-list {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          display: flex;
+          justify-content: center;
+        }
+        .nav-item {
+          margin: 0 1rem;
+        }
+        .nav-item a {
+          display: block;
+          color: white;
+          text-align: center;
+          padding: 14px 20px;
+          text-decoration: none;
+          font-family: "Inconsolata", monospace;
+          font-size: 1.2rem;
+        }
+        .nav-item a:hover {
+          background-color: #575757;
+          color: #ebba34;
+        }
+        .nav-item.active a {
+          background-color: #575757;
+          color: #ebba34;
+        }
       `}</style>
     </header>
-  )
+  );
 }

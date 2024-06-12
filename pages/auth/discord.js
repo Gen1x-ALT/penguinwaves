030718bf7190acc9a0c67ee069c307f35e6ca62b
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-12 23:47:39 +0000

    desperate problems require desperate solutions

--- pages/auth/discord.js
+++ pages/auth/discord.js
@@ -2,51 +2,51 @@ import Head from "next/head";
 import Header from "/components/Header";
 import Footer from "/components/Footer";
 import styles from "/styles/Home.module.css";
-import axios from 'axios';
+import axios from "axios";
 
 export async function getServerSideProps(context) {
   try {
-    const res = await axios.get('https://pwaves-backend.vercel.app/auth/user/info', {
-      headers: {
-        'Content-Type': 'application/json', // Set the content type to JSON
-        'Cookie': context.req.headers.cookie // Include the cookie from the request context
+    axios.defaults.withCredentials = true;
+
+    const res = await axios.get(
+      "https://pwaves-backend.vercel.app/auth/user/info",
+      {
+        headers: {
+          "Content-Type": "application/json", // Set the content type to JSON
+        },
       },
-      withCredentials: true
-    });
+    );
 
     if (res.status !== 200) {
       // Handle error, if any
-      console.error('Error fetching user info:', res.status, res.statusText);
-      return {
-        redirect: {
-          destination: '/error', // Redirect to an error page if fetching fails
-          permanent: false,
-        },
-      };
+      console.error("Error fetching user info:", res.status, res.statusText);
     }
 
     const userInfo = res.data;
 
     // Save user info in the session
     context.res.setHeader(
-      'Set-Cookie',
-      `userInfo=${JSON.stringify(userInfo)}; Path=/; HttpOnly`
+      "Set-Cookie",
+      `userInfo=${JSON.stringify(userInfo)}; Path=/; HttpOnly`,
     );
 
     // Redirect to the homepage
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
     };
   } catch (error) {
     // Log the error details in the console
-    console.error('Error fetching user info:', error.response ? error.response.data : error.message);
+    console.error(
+      "Error fetching user info:",
+      error.response ? error.response.data : error.message,
+    );
 
     return {
       redirect: {
-        destination: '/error', // Redirect to an error page if fetching fails
+        destination: "/error", // Redirect to an error page if fetching fails
         permanent: false,
       },
     };
@@ -62,7 +62,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-       <a href="/">If this page did not redirect you, click this.</a>
+        <a href="/">If this page did not redirect you, click this.</a>
       </main>
 
       <Footer />

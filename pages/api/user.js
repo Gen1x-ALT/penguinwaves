75708bc07f504b4869ad71ba5455c1292fa2d0a0
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-14 02:15:46 +0000

    trying something out...

---
+++ pages/api/user.js
@@ -0,0 +1,29 @@
+// pages/api/getToken.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const options = {
+        method: 'POST',
+        url: 'https://dev-shvzsx4icizctmai.us.auth0.com/oauth/token',
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        data: new URLSearchParams({
+          grant_type: 'client_credentials',
+          client_id: process.env.AUTH0_CLIENT_ID,
+          client_secret: process.env.AUTH0_CLIENT_SECRET,
+          audience: 'https://dev-shvzsx4icizctmai.us.auth0.com/api/v2/'
+        })
+      };
+
+      const response = await axios(options);
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(error.response?.status || 500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}

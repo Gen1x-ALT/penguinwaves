4788a102c0a802a3d015f12ce22031b1a8e3594d
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-15 18:24:08 +0000

    experimental fucked up page

---
+++ pages/_error.js
@@ -0,0 +1,37 @@
+// pages/_error.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import Error from 'next/error';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const MyError = ({ statusCode }) => {
+  return (
+    <div>
+      <Header title="Whoops... We fucked up." />
+      <p>
+        {statusCode
+          ? 'By the looks of it, this error really looks like our fault. Sorry about that... Try again later, alright?'
+          : 'By the looks of it, this error really looks like your fault. You should probably try again later, refresh this page, or contact us in our Discord Server.'}
+      </p>
+      <small>for the dev nerds:  {statusCode
+        ? 'this error occured on our end, dont worry, you are not to blame for this (server-side)'
+        : 'this is probably your fault, but it might be ours somehow (client-side)'}.<br></br>error code: {statusCode}</small>
+    </div>
+  );
+};
+
+MyError.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+MyError.propTypes = {
+  statusCode: PropTypes.number,
+};
+
+MyError.defaultProps = {
+  statusCode: null,
+};
+
+export default MyError;
--- pages/api/user.js
+++ pages/api/user.js
@@ -1,29 +1,27 @@
-// pages/api/getToken.js
 import axios from 'axios';
+import qs from 'qs';
 
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const options = {
-        method: 'POST',
-        url: 'https://dev-shvzsx4icizctmai.us.auth0.com/oauth/token',
-        headers: {'content-type': 'application/x-www-form-urlencoded'},
-        data: new URLSearchParams({
-          grant_type: 'client_credentials',
-          client_id: process.env.AUTH0_CLIENT_ID,
-          client_secret: process.env.AUTH0_CLIENT_SECRET,
-          audience: 'http://many-things.hstn.me/'
-        })
-      };
+const data = qs.stringify({
+  grant_type: 'client_credentials',
+  client_id: process.env.ACCOUNT_CLIENT_ID, // Replace with actual client ID if not using environment variables
+  client_secret: 'YOUR_CLIENT_SECRET', // Replace with your actual client secret
+  audience: `https://${process.env.ACCOUNT_NAMESPACE}/api/v2/`
+});
 
-      const response = await axios(options);
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(error.response?.status || 500).json({ error: error.message });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+const config = {
+  method: 'post',
+  url: `https://${process.env.ACCOUNT_NAMESPACE}/oauth/token`,
+  headers: { 
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  data: data
+};
+
+axios(config)
+  .then(response => {
+    res.status(200).json(response.data);
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'Server error' });
+    console.error(error);
+  });

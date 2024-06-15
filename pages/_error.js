c158e9e798539e73217db927e2b18f047ba75d3d
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-06-15 18:28:43 +0000

    this is idiotic

--- pages/_error.js
+++ pages/_error.js
@@ -1,22 +1,32 @@
 // pages/_error.js
-import React from 'react';
-import PropTypes from 'prop-types';
-import Error from 'next/error';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import PropTypes from "prop-types";
+import Error from "next/error";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const MyError = ({ statusCode }) => {
   return (
-    <div>
-      <Header title="Whoops... We fucked up." />
-      <p>
-        {statusCode
-          ? 'By the looks of it, this error really looks like our fault. Sorry about that... Try again later, alright?'
-          : 'By the looks of it, this error really looks like your fault. You should probably try again later, refresh this page, or contact us in our Discord Server.'}
-      </p>
-      <small>for the dev nerds:  {statusCode
-        ? 'this error occured on our end, dont worry, you are not to blame for this (server-side)'
-        : 'this is probably your fault, but it might be ours somehow (client-side)'}.<br></br>error code: {statusCode}</small>
+    <div className={styles.container}>
+      <Head>
+        <title>PenguinWaves</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Header title="Whoops... We fucked up." />
+        <p>
+          {statusCode
+            ? "By the looks of it, this error really looks like our fault. Sorry about that... Try again later, alright?"
+            : "By the looks of it, this error really looks like your fault. You should probably try again later, refresh this page, or contact us in our Discord Server."}
+        </p>
+        <small>
+          for the dev nerds:{" "}
+          {statusCode
+            ? "this error occured on our end, dont worry, you are not to blame for this (server-side)"
+            : "this is probably your fault, but it might be ours somehow (client-side)"}
+          .<br></br>error code: {statusCode}
+        </small>
+      </main>
     </div>
   );
 };
